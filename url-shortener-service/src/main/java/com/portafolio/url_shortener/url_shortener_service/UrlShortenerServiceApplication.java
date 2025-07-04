bdb55276370a2fe6d1a9bb8665a090cd57d5045c
TiagoV69 <santyvarela.03@gmail.com>
2025-07-03 22:05:36 -0500

    docs: Integra SpringDoc OpenAPI para documentacion automatica de la API y se añade la dependencia springdoc para generar una UI de Swagger. Configura la informacion basica de la API (titulo, version, descripcion) mediante la anotacion @OpenAPIDefinition en la clase principal de la aplicacion esto proporciona una documentacion interactiva y auto-generada, mejorando la usabilidad y mantenibilidad del servicio.

--- url-shortener-service/src/main/java/com/portafolio/url_shortener/url_shortener_service/UrlShortenerServiceApplication.java
+++ url-shortener-service/src/main/java/com/portafolio/url_shortener/url_shortener_service/UrlShortenerServiceApplication.java
@@ -3,11 +3,21 @@ package com.portafolio.url_shortener.url_shortener_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition( // Anotacion que configura la info de OpenAPI
+    info = @Info(
+        title = "API Acortador de URLs", // Aqui dejamos el titulo de nuestra API
+        version = "1.0.0", // Esta es la version de nuestra API :D
+        description = "Un microservicio simple para acortar URLs. Este proyecto demuestra los fundamentos de Spring Boot, API REST, JPA y buenas practicas de desarrollo." 
+    )
+)
 public class UrlShortenerServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UrlShortenerServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UrlShortenerServiceApplication.class, args);
+    }
 
 }
